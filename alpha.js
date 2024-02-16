93fef5d2249464bacb921bb90353cf6d54a91250
lotiful <lotiful2003@gmail.com>
2024-02-16 17:29:51 +0600

    playground complete

--- alpha.js
+++ alpha.js
@@ -23,6 +23,53 @@
 //  }
 
 //  play game
+ 
+function handleKeyboardButtonPress(event){
+   const playerPressed= event.key;
+
+   // get the expected to press
+   const currentAlphabetElement= document.getElementById('current-alphabet');
+   const currentAlphabet= currentAlphabetElement.innerText;
+   const expectedAlphabet = currentAlphabet.toLowerCase();
+   console.log(playerPressed,expectedAlphabet);
+
+   // check matched or not
+   if(playerPressed===expectedAlphabet){
+      console.log('you get a point');
+      const currentScoreElement = document.getElementById('current-score')
+      const currentScoreText = currentScoreElement.innerText;
+      const currentScore= parseInt(currentScoreText);
+      console.log(currentScore);
+
+
+      const newScore = currentScore + 1;
+      currentScoreElement.innerText = newScore;
+
+
+
+
+      removeBackgroundColorByID(expectedAlphabet);
+      continueGame();
+   }else{
+      console.log("you missed. you lost a life");
+      // step-1 get the current life number
+      const currentLifeElement= document.getElementById('current-life')
+      const currentLifeText = currentLifeElement.innerText;
+      const currentLife = parseInt(currentLifeText);
+   
+
+      // step-2 get the current life number
+      const newLife = currentLife -1;
+      // step-3 get the current life number
+      currentLifeElement.innerText= newLife;
+   }
+
+}
+// capture keyboard key press
+document.addEventListener('keyup', handleKeyboardButtonPress)
+
+
+
 function continueGame(){
     const alphabet =gatARandomAlphabet();
     console.log(alphabet);
--- utility.js
+++ utility.js
@@ -10,3 +10,7 @@ function showElementById(elementId){
     const element= document.getElementById(elementId);
     element.classList.add('bg-orange-400');
  }
+ function removeBackgroundColorByID(elementId){
+    const element= document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+ }
